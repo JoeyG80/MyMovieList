2d0ac124be9b4be80eb58b08e5fc122eebf4895e
Joseph J. Greenfield <joey0985@gmail.com>
2018-04-19 20:42:33 -0400

    Add project files.

---
+++ EFSecurityShell/DAL/BookASeatDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EFSecurityShell.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EFSecurityShell.DAL
+{
+    public class BookASeatDbContext: DbContext
+    {
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
